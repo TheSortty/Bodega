bdb8b3a2a9565e88964dbb721c11b556731b29ce
Gonzalo Murguia <102250795+TheSortty@users.noreply.github.com>
2023-10-25 15:13:23 -0300

    Cambios

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,32 +1,109 @@
 const express = require('express');
-const connection = require('../connection')
+const connection = require('../connection');
 const router = express.Router();
 
-//Uso para registrarse a la pagina
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Uso para registrarse en la página
 router.post('/signup', (req, res) => {
     let user = req.body;
-    query = "select email, password, role, status from users where email=?";
+    let query = "select email, password, role, status from users where email=?";
     connection.query(query, [user.email], (err, results) => {
         if (!err) {
             if (results.length <= 0) {
-                //query = "insert into users(name, contact, email, password, status, role) value(?,?,?,?, 'false', 'user')";
-                connection.query("insert into users(name, contact, email, password, status, role) value(?,?,?,?, 'false', 'user')",
-                [user.name, user.contact, user.email, user.password], (err, results) => {
+                query = "insert into users(name, contact, email, password, status, role) values(?,?,?,?, 'false', 'user')";
+                connection.query(query, [user.name, user.contact, user.email, user.password], (err, results) => {
                     if (!err) {
-                        return res.status(200).json({ message: "!Successfully registered!" });
+                        return res.status(200).json({ message: "Successfully registered!" });
                     } else {
                         return res.status(500).json(err);
                     }
-                })
+                });
             } else {
-                return res.status(400).json({ message: "Emaily already exist." });
+                return res.status(400).json({ message: "Email already exists." });
             }
         } else {
             return res.status(500).json(err);
         }
-    })
+    });
+});
+// Para iniciar sesión en la página
+router.post('/login', (req, res) => {
+    const user = req.body;
+    let query = "select email, password, role, status from users where email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password !== user.password) {
+                return res.status(401).json({ message: "Incorrect username or password" });
+            } else if (results[0].status === "false") {
+                return res.status(401).json({ message: "Wait for admin approval" });
+            } else {
+                const response = { email: results[0].email, role: results[0].role };
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
+                    expiresIn: "8h",
+                });
+                return res.status(200).json({ token: accessToken });
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+    });
 });
 
 
+// Correo para recuperación de contraseña
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+// Para la recuperación de contraseña
+router.post('/recoverpassword', (req, res) => {
+    const user = req.body;
+    let query = "select email, password from users where email=?";
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
+                return res.status(404).json({ message: "Email not found" });
+            } else {
+                var mailOptions = {
+                    from: process.env.EMAIL,
+                    to: results[0].email,
+                    subject: 'WINE BLOCK - ACCESS RECOVERY',
+                    html: '<p><b>Tus credenciales del sistema son los siguientes:</b> <br>  <b>EMAIL:</b> ' + results[0].email + ' <br> <b>CLAVE:</b> ' + results[0].password + ' <br> <a href="http://localhost:4200">Click para acceder al sistema</a></p>'
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        console.log("Email sent successfuly" + info.response);
+                    }
+                });
+                return res.status(200).json({ message: "Password sent successfully to your email." });
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.get('/get', (req, res)=> {
+    let query = "select id, name, email, contact, status from users where role='user'";
+    connection.query(query, (err, results)=>{
+        if(!err){
+            return res.status(200).json(results);
+        }else{
+            return res.status(500).json(err);
+        }
+    })
+})
 
 module.exports = router;
