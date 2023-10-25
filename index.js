9430d4ec7d556d98bfd81df882fa666e02461a8f
Gonzalo Murguia <102250795+TheSortty@users.noreply.github.com>
2023-10-25 16:40:16 -0300

    Arreglos

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 var cors = require('cors');
 const connection = require('./connection');
-const userRoute = require('./routes/userRoute')
+const user = require('./routes/user')
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/userRoute', userRoute);
+app.use('/user', user);
 
 
 module.exports = app;
